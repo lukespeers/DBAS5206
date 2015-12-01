c65264a8f4317e4554e2ab24984ed8a6c821005f
Daniel Wicks <dan.wicks@outlook.com>
2015-12-01 18:41:17 -0500

    Report_Generator_Started
    Started working on report generator application. I got the demo started
    within the project that was already there, and I started our actual
    report generator in a seperate project ReportGenerator.

---
+++ MVCH/MVCH/ReportDisplay.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MVCH
+{
+    public partial class frmReportDisplay : Form
+    {
+        public frmReportDisplay()
+        {
+            InitializeComponent();
+        }
+
+        // Data Set object
+        //private DBASDataSet hospitalInfoDataSet;
+        // Table Adapter Objects
+        //private DBASDataSetTableAdapters.invoicesTableAdapter invoiceTableAdapter;
+        private DBASDataSetTableAdapters.patient_admissionsTableAdapter aAdmissionsTableAdapter;
+        private DBASDataSetTableAdapters.patientsTableAdapter aPatientTableAdapter;
+        private DBASDataSetTableAdapters.physiciansTableAdapter aPhysiciansTableAdapter;
+        private DBASDataSet aDataSet;
+
+        private void frmMainMenu_Load(object sender, EventArgs e)
+        {
+            // Report object for use at run time
+            PatientPhysicianReport aPatientPhysicianReport = new PatientPhysicianReport();
+            // RoomUtilizationReport aRoomUtilizationReport;
+            try
+            {
+                aDataSet = new DBASDataSet();
+                aPatientTableAdapter = new DBASDataSetTableAdapters.patientsTableAdapter();
+                aPhysiciansTableAdapter = new DBASDataSetTableAdapters.physiciansTableAdapter();
+                aAdmissionsTableAdapter = new DBASDataSetTableAdapters.patient_admissionsTableAdapter();
+
+                aPatientTableAdapter.Fill(aDataSet.patients);
+                aPhysiciansTableAdapter.Fill(aDataSet.physicians);
+                aAdmissionsTableAdapter.Fill(aDataSet.patient_admissions);
+
+                // Set up viewer to show report object
+                rptPatientPhysicianViewer.ReportSource = aPatientPhysicianReport;
+            }
+            catch (Exception dataException)
+            {
+                MessageBox.Show(dataException.Message);
+            }
+
+            rptPatientPhysicianViewer.RefreshReport();
+        }
+    }
+}
---
+++ ReportGenerator/ReportGenerator/PatientPhysicians.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ReportGenerator
+{
+    public partial class frmPatientPhysicians : Form
+    {
+        public frmPatientPhysicians()
+        {
+            InitializeComponent();
+        }
+             // Data Set object
+        //private DBASDataSet hospitalInfoDataSet;
+        // Table Adapter Objects
+        //private DBASDataSetTableAdapters.invoicesTableAdapter invoiceTableAdapter;
+        private DBASDataSet aDataSet;
+        private DBASDataSetTableAdapters.patient_admissionsTableAdapter anAdmissionsTableAdapter;
+        private DBASDataSetTableAdapters.patientsTableAdapter aPatientTableAdapter;
+        private DBASDataSetTableAdapters.physiciansTableAdapter aPhysiciansTableAdapter;        
+
+        private void frmPatientPhysicians_Load(object sender, EventArgs e)
+        {
+            // Report object for use at run time
+            PatientPhysicianReport aPatientPhysicianReport = new PatientPhysicianReport();
+            // RoomUtilizationReport aRoomUtilizationReport;
+            try
+            {
+                aDataSet = new DBASDataSet();
+                aPatientTableAdapter = new DBASDataSetTableAdapters.patientsTableAdapter();
+                aPhysiciansTableAdapter = new DBASDataSetTableAdapters.physiciansTableAdapter();
+                anAdmissionsTableAdapter = new DBASDataSetTableAdapters.patient_admissionsTableAdapter();
+
+                aPatientTableAdapter.Fill(aDataSet.patients);
+                aPhysiciansTableAdapter.Fill(aDataSet.physicians);
+                anAdmissionsTableAdapter.Fill(aDataSet.patient_admissions);
+
+                // Set up viewer to show report object
+                rptPatientPhysicianViewer.ReportSource = aPatientPhysicianReport;
+            }
+            catch (Exception dataException)
+            {
+                MessageBox.Show(dataException.Message);
+            }
+        }
+    }
+}
